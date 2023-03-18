08f10a1a3451ff72225e2bef35346ffa41de1a0d
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-18 16:57:51 +0200

    tailwind seems to be working now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 import type { HeadFC } from 'gatsby';
 
 export default function IndexPage() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <h1 className='text-lime-500 text-3xl font-bold underline'>
+        Hello world
+      </h1>
+    </main>
+  );
 }
 
 export const Head: HeadFC = () => <title>Home Page</title>;
